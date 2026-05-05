3ccee545567096a2a5eba411bba80c8ef6da50ad
Shalini Sakthivel <shalinisakthivel1297@gmail.com>
2026-05-05 16:21:26 +0530

    DDL_Commands_Practice
    Covers SQL Data Definition Language (DDL) commands such as CREATE, ALTER, RENAME, TRUNCATE, and DROP for managing database structure.

---
+++ DDL_Commands.sql
@@ -0,0 +1,32 @@
+-- DDL Commands Practice
+
+-- Creating Database 
+
+CREATE DATABASE HospitalDB;
+USE HospitalDB; 
+
+-- CREATE TABLE
+CREATE TABLE Patients (
+    PatientID INT,
+    PatientName VARCHAR(50),
+    Age INT,
+    Gender VARCHAR(10),
+    AdmissionDate DATE
+);
+
+-- ALTER - ADD COLUMN
+ALTER TABLE Patients
+ADD DoctorAssigned VARCHAR(50);
+
+-- ALTER - MODIFY COLUMN
+ALTER TABLE Patients
+MODIFY PatientName VARCHAR(100);
+
+-- RENAME TABLE
+RENAME TABLE Patients TO Patient_Info;
+
+-- TRUNCATE TABLE
+TRUNCATE TABLE Patient_Info;
+
+-- DROP TABLE
+DROP TABLE Patient_Info;
